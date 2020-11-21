f53f059476854e0ee686332811bf662f0eadca9d
Juan Miranda <jv.miranda.fuentes@gmail.com>
2020-11-21 06:06:13 -0600

    Deleted Example of Parent/Child Component

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -56,9 +56,34 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
+                <ExampleParentComponent/>
             </div>
         );
     }
+}
+/*
+
+
+
+*/
+
+class ExampleParentComponent extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            number:333
+        }
+    }
+    render(){
+        return <ExampleChildComponent number={this.state.number} greeting="Hello World"/>;
+    }
+}
+
+class ExampleChildComponent extends Component{
+    render(){
+    return <div>{this.props.number} {this.props.greeting}</div>
+    };
 }
 
 export default Directory;
