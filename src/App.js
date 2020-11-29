b5e2f48d7cc7b7bab76c0377ee0d7d54c1280076
Juan Miranda <jv.miranda.fuentes@gmail.com>
2020-11-29 14:58:27 -0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 
 
 class App extends Component {
-
-  /*we moved the presentational component into the Main Component*/
   render() {
         return (
-            <div className="App">
-               <Main/>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
 }
